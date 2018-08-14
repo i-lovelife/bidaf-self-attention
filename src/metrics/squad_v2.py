ec4bd96e9f4b6dcd488be8bef9eed50482fb353c
nanliha <nanlihao@gmail.com>
2018-08-14 14:12:51 +1000

    sync

--- src/metrics/squad_v2.py
+++ src/metrics/squad_v2.py
@@ -15,14 +15,11 @@ class SquadMetricsV2(Metric):
     """
     def __init__(self) -> None:
         self._total_em = 0.0
-        self._total_f1 = 0.0
         self._count = 0
-        self._total_no_em = 0.0
-        self._total_no_f1 = 0.0
-        self._no_count = 0
         self._total_yes_em = 0.0
-        self._total_yes_f1 = 0.0
         self._yes_count = 0
+        self._tp = 0
+        self._tn = 0
 
     @overrides
     def __call__(self, best_span_string: str, answer_strings: List[str]) -> None:
@@ -34,24 +31,18 @@ class SquadMetricsV2(Metric):
         answer_strings : ``List[str]``
             The golden span strings, may be several
         """
-        no_answer = False
-        if not answer_strings or not answer_strings[0]:
+        self._count += 1 
+        if not answer_strings or not answer_strings[0]: # Negative
             answer_strings = ['']
-            no_answer = True
-        exact_match = max(evaluate_v2.compute_exact(answer, best_span_string) for answer in answer_strings)
-        f1_score = max(evaluate_v2.compute_f1(answer, best_span_string) for answer in answer_strings)
-        self._total_em += exact_match
-        self._total_f1 += f1_score
-        self._count += 1
-
-        if no_answer:
-            self._total_no_em += exact_match
-            self._total_no_f1 += f1_score
-            self._no_count += 1
-        else:
-            self._total_yes_em += exact_match
-            self._total_yes_f1 += f1_score
+            if not best_span_string:
+                self._tn += 1
+                self._total_em += 1
+        else: # Positive
             self._yes_count += 1
+            self._tp += 1 if best_span_string else 0
+            exact_match = max(evaluate_v2.compute_exact(answer, best_span_string) for answer in answer_strings)
+            self._total_em += exact_match
+            self._total_yes_em += exact_match
 
     @overrides
     def get_metric(self, reset: bool = False) -> Dict[str, float]:
@@ -64,12 +55,14 @@ class SquadMetricsV2(Metric):
         def ave_score(value, count):
             return value/count if count > 0 else 0
         total_em = ave_score(self._total_em, self._count)
-        total_f1 = ave_score(self._total_f1, self._count)
-        total_no_em = ave_score(self._total_no_em, self._no_count)
+        total_yes_em = ave_score(self._total_yes_em, self._yes_count)
+        tp = ave_score(self._tp, self._count)
+        tn = ave_score(self._tn, self._count)
         ret = {
                 "em": total_em,
-                "f1": total_f1,
-                "no_em": total_no_em,
+                "yes_em": total_yes_em,
+                "tp": tp,
+                "tn": tn
         }
         if reset:
             self.reset()
@@ -78,16 +71,11 @@ class SquadMetricsV2(Metric):
     @overrides
     def reset(self):
         self._total_em = 0.0
-        self._total_f1 = 0.0
         self._count = 0
-        self._total_no_em = 0.0
-        self._total_no_f1 = 0.0
-        self._no_count = 0
         self._total_yes_em = 0.0
-        self._total_yes_f1 = 0.0
         self._yes_count = 0
+        self._tp = 0
+        self._tn = 0
 
     def __str__(self):
-        return f"SquadEmAndF1V2(em={self._total_em}, f1={self._total_f1}, \
-        no_em={self._total_no_em}, no_f1={self._total_no_f1}, \
-        yes_em={self._total_yes_em}, yes_f1={self._total_yes_f1})"
+        return f"SquadEmAndF1V2(em={self._total_em})"
